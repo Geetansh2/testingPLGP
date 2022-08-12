b4a6d9fc59c08d280a4beac3e51afa3e8c8d1087
GeetanshWynk <b0270593@LTB0270593-MAC-2.local>
2022-08-12 16:10:26 +0530

    Running job in pipeline

--- groovyScripts/normalPipeline.groovy
+++ groovyScripts/normalPipeline.groovy
@@ -1,22 +1,21 @@
-options {
-    skipDefaultCheckout(true)
-}
-    stages{
-        stage('Starting job'){
-            steps{
+pipeline {
+    agent any
+    stages {
+        stage('Starting job') {
+            steps {
                 echo 'Starting job'
             }
         }
-        stage('Run testNg suite job'){
-            steps{
-              build job: 'RunGradleTestNGSuite'
-               }
+        stage('Run testNg suite job') {
+            steps {
+                build job: 'RunGradleTestNGSuite'
             }
-        }
-
-    post{
-        always{
-            emailext body: 'Summary', subject: 'Pipeline Status new', to: 'geetanshbhatia12@gmail.com'
         }
     }
 
+    post {
+        always {
+            emailext body: 'Summary', subject: 'Pipeline Status new', to: 'geetanshbhatia12@gmail.com'
+        }
+    }
+}
